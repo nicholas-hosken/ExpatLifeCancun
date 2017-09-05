c29022663407f68c513d5c8de00067b0c5838669
nicholas-hosken <nicholas.hosken@yahoo.com>
2017-09-04 22:26:28 -0500

    Added a bunch of new pages

--- js/gmaps.init.js
+++ js/gmaps.init.js
@@ -8,11 +8,11 @@ $(function () {
 
 function map() {
 
-    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": -2}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
     map = new GMaps({
 	el: '#map',
-	lat: 21.161966,
-	lng: -86.851543,
+	lat: 21.161909,
+	lng: -86.851527,
 	zoomControl: true,
 	zoomControlOpt: {
 	    style: 'SMALL',
@@ -27,11 +27,11 @@ function map() {
 	styles: styles
     });
 
-    var image = '../img/marker.png';
+    var image = 'img/marker.png';
 
     map.addMarker({
-	lat: 21.161966,
-	lng: -86.851543,
+	lat: 21.161909,
+	lng: -86.851527,
 	icon: image/* ,
 	 title: '',
 	 infoWindow: {
